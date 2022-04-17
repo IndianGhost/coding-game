bb28cf8c4660853593ed97624203cf953ce9d683
IndianGhost <achraf.bellaali@gmail.com>
2022-04-17 15:25:32 +0000

    fix warnings

--- src/test/java/com/coding/games/encryptedmessage/EncryptedMessageDemoTest.java
+++ src/test/java/com/coding/games/encryptedmessage/EncryptedMessageDemoTest.java
@@ -2,7 +2,6 @@ package com.coding.games.encryptedmessage;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
